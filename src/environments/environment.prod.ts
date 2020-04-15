7e32d0368e8baa1067bc9d7e4ea8703744ff900e
Prince <prince17.ps6@gmail.com>
2020-04-15 23:36:32 +0530

    build setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://malik-env-test.ap-south-1.elasticbeanstalk.com/webapi'
+  apiBaseUrl: 'http://malik-env.ap-south-1.elasticbeanstalk.com/webapi'
 };
